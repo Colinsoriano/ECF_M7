710a4d4e54f4beebc629c5abe2a17e58b6b4be3b
Colin <c.soriano@cci-formation.nc>
2023-08-03 09:25:14 +1100

    config file - code refactor

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,9 @@
  * Modified: prepend directory path of current file, because of this file own different ENV under between Apache and command line.
  * NOTE: please remove this comment.
  */
+
+use Phalcon\Config;
+
 defined('BASE_PATH') || define('BASE_PATH', getenv('BASE_PATH') ?: realpath(dirname(__FILE__) . '/../..'));
 defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
@@ -21,7 +24,7 @@ $localConfiguration = [
 $localConfigFileName = APP_PATH.'/config/local.php';
 if(file_exists($localConfigFileName)) include 'local.php';
 
-return new \Phalcon\Config([
+return new Config([
     'version' => '1.0',
 
     'database' => [
