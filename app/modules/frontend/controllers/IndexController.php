5030c9e0982458ca98bc94860640dc9ca9562cfc
Colin <c.soriano@cci-formation.nc>
2023-08-03 16:20:29 +1100

    index view and controller - populate select fields

--- app/modules/frontend/controllers/IndexController.php
+++ app/modules/frontend/controllers/IndexController.php
@@ -3,31 +3,60 @@ declare(strict_types=1);
 
 namespace Test\Modules\Frontend\Controllers;
 
-use Test\Models\Collaborateur;
+use Test\Models\Client;
+use Test\Models\Projet;
+use Test\Models\ChefDeProjet;
+use Test\Models\Developpeur;
 
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
-        $collaborateurs = [];
-        $collabsForTable = [];
-        foreach (Collaborateur::find() as $collaborateur)
+
+        $clientsOptions = [];
+        foreach (Client::find() as $client)
         {
             // populate for select options
-            $collaborateurs [] = [
-                'value' => $collaborateur->getId(),
-                'label' => $collaborateur->getPrenom()." ".$collaborateur->getNom(),
-            ];
-
-            // populate for table content
-            $collabsForTable [] = [
-                'prenom' => $collaborateur->getPrenom(),
-                'nom' => $collaborateur->getNom(),
-                'prime' => $collaborateur->getPrimeEmbauche(),
+            $clientsOptions [] = [
+                'value' => $client->getIdClient(),
+                'label' => "{$client->getRaisonSociale()} (type : ".($client->getSs2i() ? 'SS2I' : 'Non SS2I').")",
             ];
         }
-        $this->view->setVar('collaborateurs' , $collaborateurs);
+        $this->view->setVar('clientsOptions', $clientsOptions);
+
+        $projetsOptions = [];
+        foreach (Projet::find() as $projet)
+        {
+            // populate for select options
+            $projetsOptions [] = [
+                'value' => $projet->getId(),
+                'label' => "{$projet->getNom()} (type : {$projet->translateType()})",
+            ];
+        }
+        $this->view->setVar('projetsOptions', $projetsOptions);
+
+        $chefDeProjets = [];
+        foreach (ChefDeProjet::find() as $chefDeProjet)
+        {
+            // populate for select options
+            $chefDeProjets [] = [
+                'value' => $chefDeProjet->getId(),
+                'label' => $chefDeProjet->Collaborateur->getPrenom()." ".$chefDeProjet->Collaborateur->getNom(),
+            ];
+        }
+        $this->view->setVar('chefDeProjets' , $chefDeProjets);
+
+        $developpeurs = [];
+        foreach (Developpeur::find() as $developpeur)
+        {
+            // populate for select options
+            $developpeurs [] = [
+                'value' => $developpeur->getId(),
+                'label' => $developpeur->Collaborateur->getPrenom()." ".$developpeur->Collaborateur->getNom(),
+            ];
+        }
+        $this->view->setVar('developpeurs' , $developpeurs);
     }
 
 }
